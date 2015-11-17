df7c825b264bff591b075e98af54f9d90d011f7f
alex-icez <alex-icex@mail.ru>
2015-11-17 18:34:51 +0300

    mess

--- src/lecture_9/Test.java
+++ src/lecture_9/Test.java
@@ -7,7 +7,8 @@ enum OS {
 
 class MyOS {
 	public final static MyOS Windows = new MyOS();
-	
+	public final static MyOS UNIX = new MyOS();
+	public final static MyOS MacOS = new MyOS();
 	
 	private MyOS() {
 	}
@@ -16,6 +17,9 @@ class MyOS {
 public class Test {
 
 	public static void main(String[] args) {
+		MyOS o1 = MyOS.Windows;
+		
+		
 		OS o = OS.UNIX;
 		OS b = OS.Windows;
 
