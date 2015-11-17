d3716e14267618f3e04fb0a1f228b7c4b13e3504
alex-icez <alex-icex@mail.ru>
2015-11-17 20:25:39 +0300

    mess

--- src/lecture_9/Sample3.java
+++ src/lecture_9/Sample3.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Sample3 {
 
 	public static void main(String[] args) {
-		List<Integer> l = new ArrayList<>()//new LinkedList<>();
+		List<Integer> l = new ArrayList<>();//new LinkedList<>();
 		l.add(10);
 		l.add(434);
 		Integer r = l.get(0);
