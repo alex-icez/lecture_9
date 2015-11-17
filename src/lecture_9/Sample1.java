20ef39d73873b5bc8ce64d0171d7877433ff8b81
alex-icez <alex-icex@mail.ru>
2015-11-17 19:11:49 +0300

    mess

---
+++ src/lecture_9/Sample1.java
@@ -0,0 +1,16 @@
+package lecture_9;
+
+class Perent {
+	void print() {
+	}
+}
+
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
