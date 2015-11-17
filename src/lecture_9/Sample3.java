2dd34510e5aa4a60ebfae40e16a326e26e6d6842
alex-icez <alex-icex@mail.ru>
2015-11-17 20:24:35 +0300

    mess

--- src/lecture_9/Sample3.java
+++ src/lecture_9/Sample3.java
@@ -1,10 +1,14 @@
 package lecture_9;
 
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Sample3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<Integer> l = new LinkedList<>();
+		l.add(10);
+		l.add(434);
 	}
-
 }
