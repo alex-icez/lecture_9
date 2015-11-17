dbdf83eb9423db2a1592307ff929cdf637e3cac7
alex-icez <alex-icex@mail.ru>
2015-11-17 20:34:45 +0300

    mess

--- src/lecture_9/Sample3.java
+++ src/lecture_9/Sample3.java
@@ -17,7 +17,7 @@ public class Sample3 {
 		l.add(434);
 		Integer r = l.get(0);
 		
-		Set<Integer> s = new TreeSet<>();//new HashSet<>();
+		Set<Integer> s = LinkedHashSet<>();//new TreeSet<>();//new HashSet<>();
 		s.add(1);
 		s.add(2);
 		s.add(1);
